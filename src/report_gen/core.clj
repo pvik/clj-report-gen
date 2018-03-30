a893a77e74861f44037c24b87a3d8917bc0e6182
Praveen Vikram <praveen.vikram@kiosa.org>
2018-03-30 15:13:29 -0400

    adding dispatch routing

--- src/report_gen/core.clj
+++ src/report_gen/core.clj
@@ -3,6 +3,7 @@
   (:require [report-gen.helpers :as helpers :refer [read-report]]
             [report-gen.sql :as sql :refer [run-report]]
             [report-gen.output :as output :refer [save-xlsx]]
+            [report-gen.dispatch :as dispatch :refer [route-dispatch]]
             [clojure.tools.logging :as log]
             [clojure.tools.cli :refer [cli]]))
 
@@ -26,14 +27,20 @@
         (log/error "No Report file specified")
         (println banner)
         (System/exit 0))
-      (let [report-name (:report opts)
-            data-dir (:data-dir opts)
-            output-dir (:output-dir opts)
-            report-props (helpers/read-report report-name data-dir)
-            report-name (:name report-props)
-            report-data (sql/run-report report-props data-dir)]
+      (let [report-name        (:report opts)
+            data-dir           (:data-dir opts)
+            output-dir         (:output-dir opts)
+            report-props       (helpers/read-report report-name data-dir)
+            output-props       (:output report-props)
+            dispatch-props-vec (:dispatch report-prpos)
+            report-name        (:name report-props)
+            ;; run report
+            report-data        (sql/run-report report-props data-dir)]
         (log/info "Report Data: " report-data)
-        (case (:output report-props)
+        ;; generate output files
+        (case output-props
           :xlsx (output/save-xlsx report-name report-data output-dir)
           (log/error "INVALID OUTPUT TYPE"))
+        ;; iterate over all dispatch values
+        (map #(dispatch/route-dispatch % output-props) dispatch-props-vec)
         (log/info "Done!")))))
