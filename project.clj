ed1a10bae9b2aecae25cbdb2e461048945a3633f
Praveen Vikram <praveen.vikram@kiosa.org>
2018-03-30 13:12:39 -0400

    orcl deps

--- project.clj
+++ project.clj
@@ -14,8 +14,10 @@
                  ;; db classes
                  [com.microsoft.sqlserver/mssql-jdbc "6.2.2.jre8"]
                  [org.postgresql/postgresql "42.2.2"]
-                 ;; [com.oracle/ojdbc6 "11.2.0.3"]
-                 [com.oracle/ojdbc14 "10.2.0.4.0"]]
+                 [com.oracle/ojdbc6 "12.1.0.1-atlassian-hosted"]
+                 [oracle/ojdbc6 "11.2.0.3"]
+                 ;; [com.oracle/ojdbc14 "10.2.0.4.0"]
+                 ]
   :main ^:skip-aot report-gen.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
