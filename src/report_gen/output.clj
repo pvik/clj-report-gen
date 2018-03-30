b4fdc9db5b98a33a343680fb821ecb98541dda98
Praveen Vikram <praveen.vikram@kiosa.org>
2018-03-30 14:22:44 -0400

    moving docjure to :use from :require

--- src/report_gen/output.clj
+++ src/report_gen/output.clj
@@ -1,11 +1,11 @@
 (ns report-gen.output
   (:gen-class)
+  (:use [dk.ative.docjure.spreadsheet])
   (:require [report-gen.helpers :as helpers :refer [seq-map-to-2d-vec gen-file-name]]
-            [dk.ative.docjure.spreadsheet :as docjure]
             [clojure.tools.logging :as log]))
 
 (defn save-xlsx [report-name report-data & report-dir]
   (let [filename (helpers/gen-file-name report-name :xlsx)
-        wb (docjure/create-workbook report-name (helpers/seq-map-to-2d-vec report-data))]
+        wb (create-workbook report-name (helpers/seq-map-to-2d-vec report-data))]
     (log/info "saving " filename)
-    (docjure/save-workbook! filename wb)))
+    (save-workbook! filename wb)))
