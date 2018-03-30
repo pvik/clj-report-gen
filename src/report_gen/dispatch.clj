afde81a5dbfe61f0c43a33fc1422bd9d4f3c1731
Praveen Vikram <praveen.vikram@kiosa.org>
2018-03-30 16:31:56 -0400

    using email.edn details to send-email

--- src/report_gen/dispatch.clj
+++ src/report_gen/dispatch.clj
@@ -16,7 +16,7 @@
               {:type :attachment
                :content (java.io.File. report-file-name)}]
         email-msg {:from from :to to :subject subject :body body}]
-    (postal/send-message email-msg)))
+    (postal/send-message email-server email-msg)))
 
 (defn route-dispatch [dispatch-props report-props email-server & [report-dir]]
   (log/info "dispatch-route " dispatch-props)
