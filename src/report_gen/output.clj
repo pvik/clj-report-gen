b7c8e3eaedb3b8e265d8320fb33932b568d9a2a0
Praveen Vikram <praveen.vikram@kiosa.org>
2018-04-03 16:56:25 -0400

    using :columns from report-props

--- src/report_gen/output.clj
+++ src/report_gen/output.clj
@@ -4,9 +4,9 @@
   (:require [report-gen.helpers :as helpers :refer [seq-map-to-2d-vec gen-file-name]]
             [clojure.tools.logging :as log]))
 
-(defn save-xlsx [report-name report-data & [report-dir]]
+(defn save-xlsx [report-name report-data & [columns report-dir]]
   (let [filename (str report-dir "/" (helpers/gen-file-name report-name :xlsx))
-        data-2d-vec (helpers/seq-map-to-2d-vec report-data)
+        data-2d-vec (helpers/seq-map-to-2d-vec report-data columns)
         wb (create-workbook report-name data-2d-vec)]
     (log/info "Saving " filename)
     (save-workbook! filename wb)))
