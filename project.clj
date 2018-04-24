9ece610cdc5d2387d93d55a56c237dec49b5995a
Praveen Vikram <praveen.vikram@kiosa.org>
2018-04-24 13:32:28 -0400

    fixing project :url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject report-gen "0.1.0-SNAPSHOT"
   :description "Generic Report Generator from a SQL DB and Dispatcher"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/pvik/clj-report-gen"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
