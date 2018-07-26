02d0a27caf117c0c2fc5a70570bb0d0c90b2e54a
Praveen Vikram <praveen.vikram@kiosa.org>
2018-07-26 12:15:05 -0400

    do not gen-output or dispatch if query does not return nay records

--- src/report_gen/core.clj
+++ src/report_gen/core.clj
@@ -1,32 +1,38 @@
 (ns report-gen.core
   (:gen-class)
-  (:require [report-gen.helpers :as helpers :refer [read-report]]
-            [report-gen.sql :as sql :refer [run-report]]
-            [report-gen.output :as output :refer [gen-output]]
-            [report-gen.dispatch :as dispatch :refer [dispatch-report]]
+  (:require [clojure.tools.cli :refer [cli]]
             [clojure.tools.logging :as log]
-            [clojure.tools.cli :refer [cli]]))
+            [report-gen.dispatch :as dispatch]
+            [report-gen.helpers :as helpers]
+            [report-gen.output :as output]
+            [report-gen.sql :as sql]))
 
 (defn -main
   "report-gen application entry point"
   [& args]
-  (let [[opts args banner] (cli args
-                                ["-h" "--help" "Print this help"
-                                 :default false :flag true]
-                                ["-d" "--data-dir"  "data directory to find report and db .edn files"
-                                 :default "resources"]
-                                ["-o" "--output-dir" "directory to store generated reports"
-                                 :default "/tmp"]
-                                ["-e" "--email-server" "email server properties"
-                                 :default "resources/email.edn"]
-                                ["-r" "--report" "Report .edn file"])]
+  (let [[opts args banner]
+        (cli args
+             ["-h" "--help"
+              "Print this help"
+              :default false :flag true]
+             ["-d" "--data-dir"
+              "data directory to find report and db .edn files"
+              :default "resources"]
+             ["-o" "--output-dir"
+              "directory to store generated reports"
+              :default "/tmp"]
+             ["-e" "--email-server"
+              "email server properties"
+              :default "resources/email.edn"]
+             ["-r" "--report"
+              "Report .edn file"])]
     (when (:help opts)
       (println banner)
       (System/exit 0))
     (if (nil? (:report opts))
       (let []
         (println "ERROR: No Report file specified")
-        (log/error "No Report file specified")
+        (log/error "No Report file specified  df")
         (println banner)
         (System/exit 0))
       (let [report             (:report opts)
@@ -39,10 +45,21 @@
             dispatch-props-vec (:dispatch report-props)
             report-name        (:name report-props)
             ;; run report
-            report-data        (sql/run-report report-props data-dir)]
+            report-data        (sql/run-report report-props data-dir)
+            report-count       (count report-data)]
         (log/info "Report Data: " report-data)
-        ;; generate output files
-        (output/gen-output report-name report-data output-props columns output-dir)
-        ;; dispatch report
-        (dispatch/dispatch-report dispatch-props-vec report-props email-server-props output-dir)
+        (if (> report-data 0)
+          (do
+            ;; generate output files
+            (output/gen-output report-name
+                               report-data
+                               output-props
+                               columns
+                               output-dir)
+            ;; dispatch report
+            (dispatch/dispatch-report dispatch-props-vec
+                                      report-props
+                                      email-server-props
+                                      output-dir))
+          (log/info "No records found")
         (log/info "Done!")))))
